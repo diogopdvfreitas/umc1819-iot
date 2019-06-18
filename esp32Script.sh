a0c2ccfa941ef24a56a438d3b48b9eb102d9fa5a
Diogo Freitas <diogo.maria.freitas@gmail.com>
2019-06-18 20:53:06 +0200

    Changes to esp32 WiFi configuration
    -RasPi bash script adapted to match execution of ESP32
    -ESP32 script changed to match what the assignement asked in Ex1

--- esp32Script.sh
+++ esp32Script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+sleep 3
 echo "Script Start"
 stty -F /dev/ttyESP32 cs8 115200 ignbrk -hupcl -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
 echo "tk3" > /dev/ttyESP32
-sleep 1
+sleep 2
 echo "#teamdarmstadt" > /dev/ttyESP32
 echo "Script Ran Successfully"
 $SHELL
--- esp32WiFiConfig/esp32WiFiConfig.ino
+++ esp32WiFiConfig/esp32WiFiConfig.ino
@@ -3,77 +3,107 @@
 Preferences preferences;
 
 int LED_BUILTIN = 2;
+String recv_ssid;
+String recv_pass;
 
 void setup(){
   Serial.begin(115200);
-  Serial.println();
-    
-  String ssid;
-  String pass;
-
-  preferences.clear();
-
-  /* Start a namespace "iotk" in Read-Write mode: set second parameter to false 
-  Note: Namespace name is limited to 15 chars */
   preferences.begin("iotk", false);
-
-  /* SSID and Password inputs */
-  Serial.print("SSID: ");
-  while (Serial.available() == 0);
-  ssid = Serial.readString();
-  Serial.println(ssid);
-
-  Serial.print("Password: ");
-  while (Serial.available() == 0);
-  pass = Serial.readString();
-  Serial.println(pass);
-
-  /* Store credentials to the Preferences */
-  preferences.putString("SSID", ssid);
-  preferences.putString("Pass", pass);
-
-  /* Converting a string into a char array
-  Length (it already including the null terminator) */
-  int ssid_len = ssid.length();
-  int pass_len = pass.length();
-
-  /* Prepare the character array */
-  char ssid_array[ssid_len];
-  char pass_array[pass_len];
-
-  for(int i = 0; i < ssid_len-1; i++)
-    ssid_array[i] = ssid[i];
-      
-  ssid_array[ssid_len-1] = '\0';
-
-  for(int i = 0; i < pass_len-1; i++)
-    pass_array[i] = pass[i];
-      
-  pass_array[ssid_len-1] = '\0';
-
-  /* Connecting to WiFi */
-  WiFi.begin(ssid_array, pass_array);
-
-  /* A delay is needed so we only continue with the program until 
-  the ESP32 is effectively connected to the WiFi network */
-  while (WiFi.status() != WL_CONNECTED){
-    delay(500);
-    Serial.println(".");
+  
+  delay(2000);
+  if(receiveCredentials() == true){
+    storeCredentials();
   }
 
-  if (WiFi.status() == WL_CONNECTED){
-  	onLed_10s();
-  	Serial.println("WiFi Connected.");
+  String ssid = getStoredSSID();
+  String pass = getStoredPassword();
 
- 	Serial.print("IP Address: ");
-	Serial.println(WiFi.localIP());
+  char ssid_a[ssid.length()];
+  convertString(ssid, ssid_a);
+  char pass_a[pass.length()];
+  convertString(pass, pass_a);
+
+  /*Connecting to WiFi*/
+  WiFi.begin(ssid_a, pass_a);
+
+  /*A delay is needed so we only continue with the program untill the ESP32 is effectively connected to the WiFi network*/
+  int count = 0;
+  
+  while (WiFi.status() != WL_CONNECTED){
+    delay(1000);
+    Serial.print(".");
+
+    if(count == 20){
+      Serial.println();
+      break;
+    }
+    count++;
+  }
+  
+  if (WiFi.status() == WL_CONNECTED){	
+  	Serial.println("WiFi Connected.");     
+ 	  Serial.print("IP Address: ");
+	  Serial.println(WiFi.localIP());
+
+    onLed_10s();
   }
   else {
+    Serial.println("Connection Attempt Failed");
   	blinkLed_10s();
   }
 
   /* Close the Preferences */
   preferences.end();
+}
+
+bool receiveCredentials(){
+  /*SSID input*/
+  Serial.print("SSID: ");
+
+  int count = 0;
+  while (Serial.available() == 0){
+    delay(1000);
+    if(count == 10)
+      return false;
+    
+    count++;
+  }
+
+  recv_ssid = Serial.readString();
+  Serial.print(recv_ssid);
+
+  /*Password input*/
+  Serial.print("Password: ");
+  while (Serial.available() == 0);
+  recv_pass = Serial.readString();
+  Serial.print(recv_pass);
+
+  return true;
+}
+
+void storeCredentials(){
+  preferences.clear();
+  preferences.putString("ssid", recv_ssid);
+  preferences.putString("pass", recv_pass);
+}
+
+String getStoredSSID(){
+  String ssid = preferences.getString("ssid");
+  Serial.println("Stored SSID: " + ssid);
+  return ssid;
+}
+
+String getStoredPassword(){
+  String pass = preferences.getString("pass");
+  Serial.println("Stored Password: " + pass);
+  return pass;
+}
+
+void convertString(String str, char* res){
+  int len = str.length();
+  for(int i = 0; i < len - 1; i++)
+    res[i] = str[i];
+  res[len - 1] = '\0';
 }
 
 void onLed_10s(){
@@ -83,8 +113,11 @@ void onLed_10s(){
 
 void blinkLed_10s(){
 	pinMode(LED_BUILTIN, OUTPUT);
-  	for(int i = 0; i <= 9; i++)
-	   digitalWrite(LED_BUILTIN, HIGH); delay(500); digitalWrite(LED_BUILTIN, LOW); delay(500);
+  for(int i = 0; i <= 9; i++){
+    digitalWrite(LED_BUILTIN, HIGH); delay(500); digitalWrite(LED_BUILTIN, LOW); delay(500);
+  }
 }
 
-void loop(){}
+void loop(){
+
+}
